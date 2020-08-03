60586afd7f6a7c981ecc4496ac227ca317c0aef9
Adam Currie <Adam.Currie@mail.com>
2020-08-03 16:12:47 -0400

    made optionally generic

--- SimpleFrame/ConcurrentClosableWindowCollection.cs
+++ SimpleFrame/ConcurrentClosableWindowCollection.cs
@@ -6,25 +6,27 @@ using System.Windows.Threading;
 
 namespace SimpleFrame {
 
+    internal class ConcurrentClosableWindowCollection : ConcurrentClosableWindowCollection<Window> { }
+
     /// <summary>
     /// Thread-Safe collection of windows that can be closed from other threads.
     /// </summary>
-    internal partial class ConcurrentClosableWindowCollection {
+    internal class ConcurrentClosableWindowCollection<T> where T : Window {
         private volatile int closeAllForever = 0;//0=false
-        private readonly ThreadLocal<LinkedList<Window>> threadLocalWindows
-            = new ThreadLocal<LinkedList<Window>>(() => new LinkedList<Window>(), true);
+        private readonly ThreadLocal<LinkedList<T>> threadLocalWindows
+            = new ThreadLocal<LinkedList<T>>(() => new LinkedList<T>(), true);
 
         public bool ClosedForever
             => closeAllForever != 0;
 
-        public void Add(Window window) {
+        public void Add(T window) {
             /*
              * Dispatching to window thread so that it will be in the correct thread-local collection,
              * also this makes the combination of adding the window and hooking up the closed event
              * effectively atomic since close and the closed event are only called on the ui thread.
              */
             window.Dispatcher.BeginInvoke(() => {
-                LinkedList<Window> windowsForThisThread = threadLocalWindows.Value!;
+                LinkedList<T> windowsForThisThread = threadLocalWindows.Value!;
 
                 bool closing = false;
                 lock (windowsForThisThread) {
@@ -49,16 +51,20 @@ namespace SimpleFrame {
 
         private void WindowClosed(object? s, EventArgs e) {
             lock (threadLocalWindows.Value!) {
-                threadLocalWindows.Value.Remove((Window)s!);
+                threadLocalWindows.Value.Remove((T)s!);
             }
         }
 
-        public void CloseAll() {
-            foreach (LinkedList<Window> windowsForOneThread in threadLocalWindows.Values) {
+        public void CloseAll(Action<T>? beforeClosing = null, Action<T>? afterClosing = null) {
+            foreach (LinkedList<T> windowsForOneThread in threadLocalWindows.Values) {
                 lock (windowsForOneThread) {
-                    foreach (Window window in windowsForOneThread) {
+                    foreach (T window in windowsForOneThread) {
+                        beforeClosing?.Invoke(window);
                         window.Closed -= WindowClosed;
-                        window.Dispatcher.BeginInvoke(window.Close);
+                        window.Dispatcher.BeginInvoke(() => { 
+                            window.Close(); 
+                            afterClosing?.Invoke(window);
+                        });
                     }
                     windowsForOneThread.Clear();
                 }
