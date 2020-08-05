3642d57a40944689b3025a2a4fb9e137135c0f07
Adam Currie <Adam.Currie@mail.com>
2020-08-04 22:13:49 -0400

    async image loading

--- SimpleFrame/PhotoWindowViewModel.cs
+++ SimpleFrame/PhotoWindowViewModel.cs
@@ -1,6 +1,13 @@
-﻿using SimpleFrame.DB;
+﻿using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using SimpleFrame.DB;
 using System;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SimpleFrame {
     internal class PhotoWindowViewModel : ViewModelBase {
@@ -14,14 +21,12 @@ namespace SimpleFrame {
                 if (_persistence == value) return;
                 _persistence = value;
                 if (_persistence == false) {
-                    //todo: maybe need to check existence first?
                     using (var db = new WindowDbContext()) {
                         db.Remove(data);
                         db.SaveChanges();
                     }
                 } else {
                     using (var db = new WindowDbContext()) {
-                        //todo: maybe need to create first?
                         db.Update(data);
                         db.SaveChanges();
                     }
@@ -30,38 +35,56 @@ namespace SimpleFrame {
             }
         }
 
-        public int Left {
+        private int Left {
             get => data.Left;
             set => UpdateDbBackedProperty((x) => data.Left = x, data.Left, value);
         }
 
-        public int Top {
+        private int Top {
             get => data.Top;
             set => UpdateDbBackedProperty((x) => data.Top = x, data.Top, value);
         }
 
-        public int Width {
+        private int Width {
             get => data.Width;
             set => UpdateDbBackedProperty((x) => data.Width = x, data.Width, value);
         }
 
-        public int Height {
+        private int Height {
             get => data.Height;
             set => UpdateDbBackedProperty((x) => data.Height = x, data.Height, value);
         }
 
-        public string? Frame {
+        private string? Frame {
             get => data.Frame;
             set => UpdateDbBackedProperty((x) => data.Frame = x, data.Frame, value);
         }
 
-        public string? ImagePath {
+        private string? ImagePath {
             get => data.ImagePath;
             set => UpdateDbBackedProperty((x) => data.ImagePath = x, data.ImagePath, value);
         }
 
+        private ImageSource? _imageSource;
+        public ImageSource? ImageSource {
+            get => _imageSource;
+            private set => UpdateProperty(ref _imageSource, value); 
+        }
+
+        private string? _explicitImageLoadErrorMsg;
+        /// <summary>
+        /// Error messages are minimalistic and don't include the path string.
+        /// </summary>
+        public string? ExplicitLoadImageErrorMsg {
+            get => _explicitImageLoadErrorMsg;
+            private set => UpdateProperty(ref _explicitImageLoadErrorMsg, value);
+        }
+
         public PhotoWindowViewModel(PhotoWindowData data, bool persistent = true) {
             this.data = data;
+
+            if (data.ImagePath != null)
+                _ = ExplicitlyLoadImage(data.ImagePath);
 
             _persistence = persistent;
             if (_persistence == false) {
@@ -77,6 +100,49 @@ namespace SimpleFrame {
                     db.SaveChanges();
                 }
             }
+        }
+
+        /// <summary>
+        /// Loads the next image in the current directory with wrapping.
+        /// Doesn't fail, if we loop all the way around and we can't load anything, nothing happens.
+        /// </summary>
+        private async Task LoadNextImage() {
+            throw new NotImplementedException("todo");
+        }
+
+        /// <summary>
+        /// Loads an image and supplies information to user on failure.
+        /// If this fails <see cref="ImageSource"/> is set to null.
+        /// </summary>
+        private async Task ExplicitlyLoadImage(string path) {
+            try {
+                ImageSource = await LoadBitmapImageAsync(path);
+            } catch (Exception e) {
+                ImageSource = null;
+                ExplicitLoadImageErrorMsg = e switch
+                {
+                    UriFormatException _ => "invalid file path",
+                    FileNotFoundException _ => "file not found",
+                    _ => "unknown error",
+                    //todo: there's got to be more exceptions wtf, test this out
+                };
+            }
+        }
+
+        /// <summary>
+        /// Loads an image file as a BitmapImage.
+        /// </summary>
+        /// <exception cref="FileNotFoundException">File not found.</exception>
+        /// <exception cref="UriFormatException">File not found.</exception>
+        /// <param name="path">Path of image to load.</param>
+        /// <returns></returns>
+        private async Task<BitmapImage> LoadBitmapImageAsync(string path) {
+            return await Task.Run(() => {
+                //todo: maybe pass uri around instead and have it validated much earlier
+                BitmapImage bitmap = new BitmapImage(new Uri(path));
+                bitmap.Freeze();
+                return bitmap;
+            }).ConfigureAwait(false);
         }
 
         public PhotoWindowViewModel(bool persistent = true)
