d659cdf177469cc3eb830cb5f70c42ecdacf7352
Adam Currie <Adam.Currie@mail.com>
2020-08-26 17:57:01 -0400

    repeating sides

--- PhotoFrames/FrameRendering.cs
+++ PhotoFrames/FrameRendering.cs
@@ -1,83 +1,114 @@
-﻿namespace PhotoFrames {
+﻿using System.Runtime.CompilerServices;
+
+namespace PhotoFrames {
     public static class FrameRendering {
 
-        public static unsafe void DrawHorizontalSide(FrameData.Section s, byte* dest, int destXStart, int destYStart, int writeWidth, int destStride)
-            => DrawPixelsStrechX((byte*)s.Pixels.ToPointer(), dest, destXStart, destYStart, s.Width, s.Height, writeWidth, s.BytesPerPixel, destStride);
+        public static unsafe void DrawHorizontalSide(FrameData.Section s, byte* dest, int destXStart, int destYStart, int writeWidth, int destStride) {
+            var src = (byte*)s.Pixels.ToPointer();
+            int srcStride = s.Width * s.BytesPerPixel;
 
-        public static unsafe void DrawVerticalSide(FrameData.Section s, byte* dest, int destXStart, int destYStart, int writeHeight, int destStride)
-            => DrawPixelsStrechY((byte*)s.Pixels.ToPointer(), dest, destXStart, destYStart, s.Width, s.Height, writeHeight, s.BytesPerPixel, destStride);
-
-        public static unsafe void DrawCorner(FrameData.Section s, byte* dest, int destX, int destY, int destStride)
-            => DrawPixels((byte*)s.Pixels.ToPointer(), dest, destX, destY, s.Width, s.Height, s.BytesPerPixel, destStride);
-
-        private static unsafe void DrawPixelsStrechX(byte* src, byte* dest, int destXStart, int destYStart, int srcWidth, int srcHeight, int writeWidth, int bytesPerPixel, int destStride) {
-            int srcStride = srcWidth * bytesPerPixel;
-            if (writeWidth > srcWidth) {
-                var extrapolater = new FastUpsampler(srcWidth, writeWidth);
-                for (int y = 0; y < srcHeight; y++) {
+            if (s.Repeating) {
+                int repeatCount = (writeWidth + s.Width - 1) / s.Width;
+                var interp = new SimpleResampler(s.Width*repeatCount, writeWidth);
+                int prevDestIndex = 0;
+                var averager = new PixelAverager(s.BytesPerPixel);
+                for (int y = 0; y < s.Height; y++) {
+                    for (int x = 0; x < s.Width*repeatCount; x++) {
+                        int destX = interp.Map(x);
+                        int destIndex = (destX + destXStart) * s.BytesPerPixel + (y + destYStart) * destStride;
+                        if (prevDestIndex != destIndex && averager.Count != 0)
+                            averager.WriteAverage(dest + prevDestIndex);
+                        prevDestIndex = destIndex;
+                        averager.Add(src + y * srcStride + (x%s.Width) * s.BytesPerPixel);
+                    }
+                    averager.WriteAverage(dest + prevDestIndex);
+                }
+            } else if (writeWidth > s.Width) {
+                var extrapolater = new FastUpsampler(s.Width, writeWidth);
                 for (int x = 0; x < writeWidth; x++) {
                     extrapolater.Map(x, out int sampleXa, out int sampleXb);
-                    for (int i = 0; i < bytesPerPixel; i++)
-                        dest[(y + destYStart) * destStride + (x + destXStart) * bytesPerPixel + i] = (byte)((
-                            src[y * srcStride + sampleXa * bytesPerPixel + i] +
-                            src[y * srcStride + sampleXb * bytesPerPixel + i]) / 2);
-                }}
+                    for (int y = 0; y < s.Height; y++) {
+                        for (int i = 0; i < s.BytesPerPixel; i++)
+                            dest[(y + destYStart) * destStride + (x + destXStart) * s.BytesPerPixel + i] = (byte)((
+                                src[y * srcStride + sampleXa * s.BytesPerPixel + i] +
+                                src[y * srcStride + sampleXb * s.BytesPerPixel + i]) / 2);
+                    }
+                }
             } else {
-                var interp = new FastDownsampler(srcWidth, writeWidth);
+                var interp = new SimpleResampler(s.Width, writeWidth);
                 int prevDestIndex = 0;
-                for (int y = 0; y < srcHeight; y++) {
-                    var averager = new PixelAverager(bytesPerPixel);
-                    for (int x = 0; x < srcWidth; x++) {
+                var averager = new PixelAverager(s.BytesPerPixel);
+                for (int y = 0; y < s.Height; y++) {
+                    for (int x = 0; x < s.Width; x++) {
                         int destX = interp.Map(x);
-                        int destIndex = (destX + destXStart) * bytesPerPixel + (y + destYStart) * destStride;
+                        int destIndex = (destX + destXStart) * s.BytesPerPixel + (y + destYStart) * destStride;
                         if (prevDestIndex != destIndex && averager.Count != 0)
                             averager.WriteAverage(dest + prevDestIndex);
                         prevDestIndex = destIndex;
-                        averager.Add(src + y * srcStride + x * bytesPerPixel);
+                        averager.Add(src + y * srcStride + x * s.BytesPerPixel);
                     }
                     averager.WriteAverage(dest + prevDestIndex);
                 }
             }
         }
 
-        private static unsafe void DrawPixelsStrechY(byte* src, byte* dest, int destXStart, int destYStart, int srcWidth, int srcHeight, int writeHeight, int bytesPerPixel, int destStride) {
-            int srcStride = srcWidth * bytesPerPixel;
-            if (writeHeight > srcHeight) {
-                var extrapolater = new FastUpsampler(srcHeight, writeHeight);
+        public static unsafe void DrawVerticalSide(FrameData.Section s, byte* dest, int destXStart, int destYStart, int writeHeight, int destStride) {
+            var src = (byte*)s.Pixels.ToPointer();
+            int srcStride = s.Width * s.BytesPerPixel;
+
+            if (s.Repeating) {
+                int repeatCount = (writeHeight + s.Height - 1) / s.Height;
+                var interp = new SimpleResampler(s.Height*repeatCount, writeHeight);
+                int prevDestIndex = 0;
+                var averager = new PixelAverager(s.BytesPerPixel);
+                for (int y = 0; y < s.Height*repeatCount; y++) {
+                    int destY = interp.Map(y);
+                    for (int x = 0; x < s.Width; x++) {
+                        int destIndex = (x + destXStart) * s.BytesPerPixel + (destY + destYStart) * destStride;
+                        if (prevDestIndex != destIndex && averager.Count != 0)
+                            averager.WriteAverage(dest + prevDestIndex);
+                        prevDestIndex = destIndex;
+                        averager.Add(src + (y%s.Height) * srcStride + x * s.BytesPerPixel);
+                    }
+                    averager.WriteAverage(dest + prevDestIndex);
+                }
+            } else if (writeHeight > s.Height) {
+                var extrapolater = new FastUpsampler(s.Height, writeHeight);
                 for (int y = 0; y < writeHeight; y++) {
                     extrapolater.Map(y, out int sampleYa, out int sampleYb);
-                    for (int x = 0; x < srcWidth; x++) {
-                    for (int i = 0; i < bytesPerPixel; i++)
-                        dest[(y + destYStart) * destStride + (x + destXStart) * bytesPerPixel + i] = (byte)((
-                            src[sampleYa * srcStride + x * bytesPerPixel + i] +
-                            src[sampleYb * srcStride + x * bytesPerPixel + i]) / 2);
+                    for (int x = 0; x < s.Width; x++) {
+                        for (int i = 0; i < s.BytesPerPixel; i++)
+                            dest[(y + destYStart) * destStride + (x + destXStart) * s.BytesPerPixel + i] = (byte)((
+                                src[sampleYa * srcStride + x * s.BytesPerPixel + i] +
+                                src[sampleYb * srcStride + x * s.BytesPerPixel + i]) / 2);
                     }
                 }
             } else {
-                var interp = new FastDownsampler(srcHeight, writeHeight);
+                var interp = new SimpleResampler(s.Height, writeHeight);
                 int prevDestIndex = 0;
-                for (int y = 0; y < srcHeight; y++) {
+                var averager = new PixelAverager(s.BytesPerPixel);
+                for (int y = 0; y < s.Height; y++) {
                     int destY = interp.Map(y);
-                    var averager = new PixelAverager(bytesPerPixel);
-                    for (int x = 0; x < srcWidth; x++) {
-                        int destIndex = (x + destXStart) * bytesPerPixel + (destY + destYStart) * destStride;
+                    for (int x = 0; x < s.Width; x++) {
+                        int destIndex = (x + destXStart) * s.BytesPerPixel + (destY + destYStart) * destStride;
                         if (prevDestIndex != destIndex && averager.Count != 0)
                             averager.WriteAverage(dest + prevDestIndex);
                         prevDestIndex = destIndex;
-                        averager.Add(src + y * srcStride + x * bytesPerPixel);
+                        averager.Add(src + y * srcStride + x * s.BytesPerPixel);
                     }
                     averager.WriteAverage(dest + prevDestIndex);
                 }
             }
         }
 
-        private static unsafe void DrawPixels(byte* src, byte* dest, int destX, int destY, int srcWidth, int srcHeight, int bytesPerPixel, int destStride) {
-            int srcStride = srcWidth * bytesPerPixel;
-            for (int y = 0; y < srcHeight; y++)
-            for (int x = 0; x < srcWidth; x++)
-            for (int i = 0; i < bytesPerPixel; i++)
-                dest[(y + destY) * destStride + (x + destX) * bytesPerPixel + i]
-                    = src[y * srcStride + x * bytesPerPixel + i];
+        public static unsafe void DrawCorner(FrameData.Section s, byte* dest, int destX, int destY, int destStride) {
+            var src = (byte*)s.Pixels.ToPointer();
+            int srcStride = s.Width * s.BytesPerPixel;
+            for (int y = 0; y < s.Height; y++)
+            for (int x = 0; x < s.Width; x++)
+            for (int i = 0; i < s.BytesPerPixel; i++)
+                dest[(y + destY) * destStride + (x + destX) * s.BytesPerPixel + i]
+                    = src[y * srcStride + x * s.BytesPerPixel + i];
         }
 
     }
